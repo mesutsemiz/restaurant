06e61d0edcd2296f0c1a15596fd73ce14f7a500e
Mesut <semihturgutlu@gmail.com>
2024-01-12 23:49:57 +0100

    menu and navbar comp. is created partly

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
+import Menu from './Menu'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase' >
+      <div>
+        {/* LOGO */}
+        <Link href="/">Massimo</Link>
+      </div>
+      {/* MOBIL MENU */}
+      <div>
+        <Menu/>
+      </div>
+    </div>
   )
 }
 
